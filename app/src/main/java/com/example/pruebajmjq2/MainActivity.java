6cc20127eabd4e7ad8da6b96d3c62b58c33b24bc
José Francisco Quintero Moreno <josequinteromoreno13@gmail.com>
2020-03-23 11:46:13 +0100

    boton

--- app/src/main/java/com/example/pruebajmjq2/MainActivity.java
+++ app/src/main/java/com/example/pruebajmjq2/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.pruebajmjq2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button JButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
